694d794b242f38df376d90e185c5188bfc43cb1e
kyh885 <kyh885@gmail.com>
2021-12-19 15:49:30 +0900

    ch05추가

---
+++ Part3/ch05_moreAboutSpring/src/main/java/com/example/ch05_moreaboutspring/Ch05MoreAboutSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.ch05_moreaboutspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch05MoreAboutSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch05MoreAboutSpringApplication.class, args);
+    }
+
+}
---
+++ Part3/ch05_moreAboutSpring/src/test/java/com/example/ch05_moreaboutspring/Ch05MoreAboutSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ch05_moreaboutspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch05MoreAboutSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
