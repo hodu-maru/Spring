7b5342e312969b4cc2dd96ddc075e85ebd72cb54
kyh885 <kyh885@gmail.com>
2021-12-30 23:59:50 +0900

    exception프로젝트 생성

---
+++ Part3/ch06/handleException/src/main/java/com/example/handleexception/controller/ApiController.java
@@ -0,0 +1,34 @@
+package com.example.handleexception.controller;
+
+import com.example.handleexception.dto.User;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@RestController
+@RequestMapping("/api/user")
+@Validated
+public class ApiController {
+
+    @GetMapping("") // ?name=1234
+    public User get(@RequestParam(required = false) String name, @RequestParam(required = false) Integer age) {
+        User user = new User();
+        user.setName(name);
+        user.setAge(age);
+
+        int a = 10 + age;
+
+        return user;
+    }
+
+
+    @PostMapping("")
+    public User post(@Valid @RequestBody User user) {
+        System.out.println(user);
+        return user;
+    }
+}
